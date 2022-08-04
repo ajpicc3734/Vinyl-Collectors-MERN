6d37f916297672babbfdad05bb531a76a176a762
ajpicc3734 <99152798+ajpicc3734@users.noreply.github.com>
2022-08-03 20:29:56 -0400

    fixed apollo server bugs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,23 +2,29 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-type User {
-  _id: ID
-  username: String
-  email: String
-  records: [Record]
-  comments: [Comment]
-}
+  type User {
+    _id: ID
+    username: String
+    email: String
+    records: [Record]
+    comments: [Comment]
+  }
 
+  type Comment {
+    _id: ID
+    commentText: String
+    username: String
+    createdAt: String
+  }
 
-type Comment {
-  _id: ID
-  commentText: String
-  username: String
-  createdAt: String
-}`
+  type Record {
+    title: String
+  }
 
-;
+  type Query {
+    helloWorld: String
+  }
+`;
 
 // export the typeDefs
 
