2e9975fc712e95e9bc206dd8bb21b6c5f10d96a5
Lu Summers <lusummers@users.noreply.github.com>
2022-08-05 21:59:49 -0400

    queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,5 @@
 //To Do
+import { gql } from '@apollo/client';
 
 //Query User
 export const QUERY_USER = gql`
@@ -22,7 +23,70 @@ export const QUERY_USER = gql`
 `;
 
 //Query Record
-//comment within record query
+export const QUERY_RECORD = gql`
+  query record($id: ID!) {
+    record(_id: $id) {
+      _id
+      title
+      artist
+      comments {
+        commentText
+        _id
+        createdAt
+        username
+      }
+    }
+  }
+`;
+export const QUERY_RECORDS= gql`
+  query records($username: String) {
+    records(username: $username) {
+        _id
+        title
+        artist
+        comments {
+          commentText
+          _id
+          createdAt
+          username
+      }
+    }
+  }
+`;
 
-//Query ME
+//Query ME 
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      records {
+        _id
+        title
+        artist
+      }
+      comments{
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+//Query ME BASIC
+
+export const QUERY_ME_BASIC = gql`
+  {
+    me {
+      _id
+      username
+      email
+      records
+      }
+    }
+  }
+`;
+
 
