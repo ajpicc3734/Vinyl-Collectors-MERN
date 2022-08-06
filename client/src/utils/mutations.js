32fad956b5c30e150dc3bd4441e04744a17ab12e
Lu Summers <lusummers@users.noreply.github.com>
2022-08-05 22:44:37 -0400

    mutations- record and comment

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,34 @@
-//add user
+import { gql } from '@apollo/client';
+
+
+
 //login user
+//add user
 //add comments
+export const ADD_COMMENT = gql`
+  mutation addComment($commentId:ID!$commentText: String!) {
+    addComment(commentId: $commentId, commentText: $commentText) {
+      _id
+      comments {
+        _id
+        commentText
+        createdAt
+        username
+      }
+    }
+  }
+`;
 //add records
+export const ADD_RECORD = gql`
+  mutation addRecord($title: String!) {
+    addRecord(title: $title) {
+      _id
+      title
+      artist
+      username
+      comments {
+        _id
+      }
+    }
+  }
+`;
