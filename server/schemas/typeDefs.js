d467d316d86455da9e3893145d11e3c409a37c32
M. Patel <patelmihir000@gmail.com>
2022-08-05 21:25:48 -0400

    manually updating files since teammates branch is not able to commit properly

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,34 +1,54 @@
-const { gql } = require("apollo-server-express");
+const { gql } = require('apollo-server-express');
 
-// create our typeDefs
 const typeDefs = gql`
   type User {
     _id: ID
     username: String
     email: String
-    records: [Record]
-    comments: [Comment]
+    friendCount: Int
+    posts: [Post]
+    # records: [Collection]
+    friends: [User]
   }
-
-  type Comment {
+  type Post {
     _id: ID
-    commentText: String
-    username: String
+    postText: String
     createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
   }
-
-  type Record {
+  
+  # type Record {
+  #   _id: ID
+  #   title: String
+  #   artist: String
+  #   comments: [Comment]
+  # }
+  type Reaction {
     _id: ID
-    title: String
-    artist: String
-    comments: [Comment]
+    reactionBody: String
+    createdAt: String
+    username: String
   }
-
+  type Auth {
+    token: ID!
+    user: User
+  }
   type Query {
-    helloWorld: String
+    me: User
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addPost(postText: String!): Post
+    addReaction(postId: ID!, reactionBody: String!): Post
+    addFriend(friendId: ID!): User
   }
 `;
-
-// export the typeDefs
 
 module.exports = typeDefs;
