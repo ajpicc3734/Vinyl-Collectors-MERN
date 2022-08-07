c2759adedb6c7eec709cad4d83a2290ac94169da
Lu Summers <lusummers@users.noreply.github.com>
2022-08-07 16:37:08 -0400

    pdated resolvers/typeDefs/models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,7 @@ const typeDefs = gql`
     email: String
     friendCount: Int
     posts: [Post]
-    # records: [Collection]
+    records: [Record]
     friends: [User]
   }
   type Post {
@@ -19,6 +19,12 @@ const typeDefs = gql`
     reactions: [Reaction]
   }
   
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
   type Record {
     _id: ID
     title: String
@@ -26,16 +32,9 @@ const typeDefs = gql`
      comments: [Comment]
    }
 
-   
    type Comment {
     _id: ID
     commentText: String
-    createdAt: String
-    username: String
-  }
-  type Reaction {
-    _id: ID
-    reactionBody: String
     createdAt: String
     username: String
   }
@@ -50,15 +49,19 @@ const typeDefs = gql`
     records(username: String): [Record]
     posts(username: String): [Post]
     post(_id: ID!): Post
+    record(_id: ID!): Record
+
   }
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!):Auth
     addPost(postText: String!): Post
     addReaction(postId: ID!, reactionBody: String!): Post
     addFriend(friendId: ID!): User
+    addRecord(title: String!, artist: String!): Record
+    addComment(recordId: ID!, commentText: String!): Record
+
   }
 `;
 
 module.exports = typeDefs;
-  // addRecord()
